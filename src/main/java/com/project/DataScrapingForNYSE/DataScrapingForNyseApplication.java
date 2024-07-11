5e6eb4d8a56161a3dbf21af56cbdc2043068382d
ilia Iliev <ilia.dimchev.iliev@gmail.com>
2024-07-11 20:19:02 +0300

    First commit

---
+++ src/main/java/com/project/DataScrapingForNYSE/DataScrapingForNyseApplication.java
@@ -0,0 +1,13 @@
+package com.project.DataScrapingForNYSE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataScrapingForNyseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataScrapingForNyseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/DataScrapingForNYSE/DataScrapingForNyseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.DataScrapingForNYSE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataScrapingForNyseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
